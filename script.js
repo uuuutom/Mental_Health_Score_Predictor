522e30241a536ca1aa7536bf6a9c4ded5e964574
uuuutom <uttamvaghasiya21@gmail.com>
2026-08-17 22:51:46 +0530

    Fix everything_3

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
         switchView('loading');
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/predict', {
+            const response = await fetch('https://mental-health-score-predictor-ovfy.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
