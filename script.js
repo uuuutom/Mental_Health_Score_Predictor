1c2cb0a3ab111458d4d2676b5bf5734e1d7043a5
uuuutom <uttamvaghasiya21@gmail.com>
2026-08-18 01:32:21 +0530

    Updated everything today

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
         switchView('loading');
 
         try {
-            const response = await fetch('https://mental-health-score-predictor-a029.onrender.com', {
+            const response = await fetch('https://mental-health-score-predictor-a029.onrender.com/predict', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
